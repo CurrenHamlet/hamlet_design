4d7ea636b70caf7d1228267d02695570242da772
Curren Hamlet <currenhamlet@gmail.com>
2026-05-19 22:38:57 -0400

    Refinements to formatting - Added colors themes to globals.css

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,15 +1,13 @@
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-16 py-6 bg-[#F5F1E8]">
-      <span className="text-[#1C3320] font-semibold text-lg">
-        Curren Hamlet
-      </span>
+    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between px-16 py-6">
+      <span className="text-heading text-lg">Curren Hamlet</span>
       <ul className="flex gap-10">
         {["Work", "About", "Blog", "Services", "Contact"].map((item) => (
           <li key={item}>
             <a
               href={`#${item.toLowerCase()}`}
-              className="text-[#1C3320] hover:opacity-60 transition-opacity"
+              className="text-heading hover:opacity-60 transition-opacity"
             >
               {item}
             </a>
